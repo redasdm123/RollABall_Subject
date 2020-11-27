5145e61e95d7205291c24aa2f9f80ed821f77d2c
redasdm123 <64580710+redasdm123@users.noreply.github.com>
2020-11-27 16:01:03 +0900

    과제

---
+++ Subject/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            Destroy(collision.gameObject,1);
+            FindObjectOfType<Text>().text = "죽었습니다.";
+        }
+    }
+}
---
+++ Subject/PickUps_sub.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUps_sub : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FindObjectOfType<Text>().text = "닿았습니다.";
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        FindObjectOfType<Text>().text = "";
+    }
+}
